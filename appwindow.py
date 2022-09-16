3fb2ca3db98ec29f5dc2f245584fe0888ca92219
artikov <oybekartikov@icloud.com>
2022-09-16 15:29:46 +0500

    GUI done

--- appwindow.py
+++ appwindow.py
@@ -12,14 +12,35 @@ class TypeSpeedGUI:
 
         self.frame = tk.Frame(self.window)
 
-        self.sample_label = tk.Label(self.window, text=random.choice(self.text), font=('Ariel', 18))
+        self.sample_label = tk.Label(self.window, text=random.choice(self.text), font=('Arial', 18))
         self.sample_label.grid(row=0, column=0, columnspan=2, padx=5, pady=5)
 
-        self.input_entry = tk.Entry(self.frame, width=40, font=('Ariel', 24))
+        self.input_entry = tk.Entry(self.frame, width=40, font=('Arial', 24))
         self.input_entry.grid(row=1, column=0, columnspan=2, padx=5, pady=10)
 
         # function to start automatically when key is pressed
-        # self.input_entry.bind('<KeyPress>', self.start)
+        self.input_entry.bind('<KeyPress>', self.start)
+
+        # timer label
+        self.speed_label = tk.Label(self.frame, text="Speed: \n"
+                                                     "0.00 CPS \n"
+                                                     "0.00 CPM \n"
+                                                     "0.00 WPS \n"
+                                                     "0.00 WPM",
+                                    font=('Arial', 18))
+        self.speed_label.grid(row=2, column=0, columnspan=2, padx=5, pady=10)
+
+        # reset button
+        self.reset_button = tk.Button(self.frame, text='Reset', command=self.reset, font=('Arial', 24))
+        self.reset_button.grid(row=3, column=0, columnspan=2, padx=5, pady=10)
+
+        self.frame.pack(expand=True)
+
+        # check if app is started or not
+        self.counter = 0
+        self.running = False
+
+
 
 
 
